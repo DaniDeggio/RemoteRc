fa081ffc21a84890564226695e829e099af14336
DanyDeggio <danideg12@gmail.com>
2024-11-04 23:32:15 +0100

    test rasp stream

--- Rasp/srcs/Rasp.cpp
+++ Rasp/srcs/Rasp.cpp
@@ -36,7 +36,13 @@ void startVideoStream() {
 		// execlp("ffmpeg", "ffmpeg", "-f", "v4l2", "-i", "/dev/video0", "-f", "mpegts", "udp://192.168.1.25:1234", NULL); //QUESTO FUNZIONA
 		
 		
-		execlp("ffmpeg", "ffmpeg", "-f", "v4l2", "-i", "/dev/video0", "-preset", "ultrafast", "-tune", "zerolatency", "-f", "mpegts", "udp://192.168.1.25:1234", NULL); 
+		execlp("ffmpeg", "ffmpeg", 
+    "-f", "v4l2", 
+    "-i", "/dev/video0", 
+    "-pix_fmt", "yuyv422",  // Specifica il formato pixel desiderato
+    "-f", "mpegts", 
+    "udp://192.168.1.25:1234", NULL);
+		// execlp("ffmpeg", "ffmpeg", "-f", "v4l2", "-i", "/dev/video0", "-preset", "ultrafast", "-tune", "zerolatency", "-f", "mpegts", "udp://192.168.1.25:1234", NULL); 
 		// execlp("ffmpeg", "ffmpeg", "-f", "v4l2", "-i", "/dev/video0", "-vcodec", "libx264", "-f", "mpegts", "udp://192.168.1.25:1234", NULL);
 	// execlp("ffmpeg", "ffmpeg", "-f", "v4l2", "-i", "/dev/video0", "-vcodec", "libx264", "-preset", "ultrafast", "-tune", "zerolatency", "-s", "640x480", "-f", "mpegts", "udp://192.168.1.25:1234", NULL);
         
