0116c57d576d0ef8f8e6f9f65dfcc262f1da1044
DanyDeggio <danideg12@gmail.com>
2024-12-10 22:41:06 +0100

    rasp cam

--- Rasp/srcs/Cam.cpp
+++ Rasp/srcs/Cam.cpp
@@ -1,59 +1,21 @@
 #include "../include/rrc_rasp.hpp"
 
-void encodeAndSendFrame(cv::Mat &frame, x264_t *encoder, x264_picture_t *pic_in, int sock, struct sockaddr_in &client_addr) {
-    x264_nal_t *nals;
-    int i_nals;
-
-    // Convert frame from BGR to YUV (I420)
-    cv::Mat yuv_frame;
-    cv::cvtColor(frame, yuv_frame, cv::COLOR_BGR2YUV_I420);
-
-    // Fill x264_picture with YUV data
-    pic_in->img.plane[0] = yuv_frame.data;
-    pic_in->img.i_stride[0] = yuv_frame.step[0];
-
-    // Encode the frame
-    int frame_size = x264_encoder_encode(encoder, &nals, &i_nals, pic_in, NULL);
-
-    if (frame_size > 0) {
-        // Send each NAL unit via UDP
-        for (int i = 0; i < i_nals; ++i) {
-            sendto(sock, nals[i].p_payload, nals[i].i_payload, 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
-        }
-    }
-}
-
 // Usa raspicam_cv per configurare la telecamera e acquisire i frame
-void startVideoStream(int sock, struct sockaddr_in &client_addr, x264_t *encoder, x264_picture_t *pic_in) {
+void startVideoStream(int sock, struct sockaddr_in &client_addr) {
     std::lock_guard<std::mutex> lock(stream_mutex);
 
-    // Inizializza RaspiCam
-    raspicam::RaspiCam_Cv camera;
-    camera.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-    camera.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-    camera.set(cv::CAP_PROP_FORMAT, CV_8UC3);  // Formato BGR per OpenCV
+    // Prepara il comando per avviare rpicam-vid con streaming su UDP
+    std::string command = "rpicam-vid -t 0 --inline -o udp://" + 
+                          std::string(inet_ntoa(client_addr.sin_addr)) + ":" + 
+                          std::to_string(ntohs(client_addr.sin_port));
 
-    // Apri la telecamera
-    if (!camera.open()) {
-        std::cerr << "Errore nell'apertura della telecamera!" << std::endl;
+    // Esegui il comando per avviare il flusso
+    int result = system(command.c_str());
+    if (result != 0) {
+        std::cerr << "Errore nell'esecuzione del comando rpicam-vid!" << std::endl;
         return;
     }
-
-    cv::Mat frame;
-
-    while (true) {
-        // Cattura il frame dalla telecamera
-        camera.grab();
-        camera.retrieve(frame);
-
-        // Codifica e invia il frame via UDP
-        encodeAndSendFrame(frame, encoder, pic_in, sock, client_addr);
-    }
-
-    // Rilascia la telecamera
-    camera.release();
 }
-
 
 void stopVideoStream() {
     // Logic to stop video stream if needed (not implemented in this version)
